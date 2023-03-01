8608a6d9741bc79ade8b8d56940173693e4b4423
LINGARAJ <lingaraj01052002@gmail.com>
2023-03-01 10:24:40 +0530

    adding.

---
+++ main.js
@@ -0,0 +1,12 @@
+var i=0;
+
+function plus(){
+	let sum = document.getElementById('box').value= ++i;
+}
+function minus(){
+		if (i>0){
+		i--;
+	}
+	let sum = document.getElementById('box').value= i;
+}
+
